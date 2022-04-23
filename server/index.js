7406c15678fb824a9a5542ac6164969baed3be2e
Mercury06 <zetmail@ukr.net>
2022-04-23 23:04:01 +0300

    models commit

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ const app = express()
 
 const start = async () => {
     try {
+        await sequelize.authenticate()
+        await sequelize.sync()
         app.listen(PORT, () => console.log (`server started on port ${PORT}`))
     } catch (e) {
         console.log(e)
